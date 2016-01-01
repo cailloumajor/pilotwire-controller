0425b8f834a02eeee2b257fc68b714d69c60fdcc
Arnaud Rocher <arnaud.roche3@gmail.com>
2016-01-01 20:57:18 +0100

    Add pilotwire controller client code

---
+++ pilotwire_controller/client.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+from cached_property import cached_property
+from xmlrpc.client import ServerProxy, Fault as XMLRPCFault
+
+from .zeroconf import ServiceDiscoveryClient, ZeroconfServiceNotFound
+
+
+class PilotwireModesInconsistent(Exception):
+    pass
+
+
+class ControllerProxy:
+
+    def __init__(self, zeroconf_timeout=None):
+        self.zeroconf_timeout = zeroconf_timeout
+
+    @cached_property
+    def _xmlrpc_client(self):
+        zeroconf_service = ServiceDiscoveryClient(self.zeroconf_timeout)
+        return ServerProxy(
+            'http://{s.address}:{s.port}/'.format(s=zeroconf_service)
+        )
+
+    @property
+    def modes(self):
+        return self._xmlrpc_client.getModes()
+
+    @modes.setter
+    def modes(self, modes_dict):
+        response = self._xmlrpc_client.setModes(modes_dict)
+        if not all(item in response.items() for item in modes_dict.items()):
+            raise PilotwireModesInconsistent(
+                "Modes on controller not reflecting requested modes"
+            )
+
+    def test(self):
+        return self._xmlrpc_client.test()
+
+    def check_status(self):
+        # if '_xmlrpc_client' in self.__dict__:
+        #     del self._xmlrpc_client
+
+        socket.setdefaulttimeout(3)
+
+        try:
+            result = self.test()
+        except ZeroconfServiceNotFound:
+            return 'service_not_found'
+        except ConnectionError:
+            return 'connection_error'
+        except XMLRPCFault:
+            return 'xml-rpc_error'
+        except:
+            return 'unknown_error'
+        else:
+            if result == True:
+                return 'active'
+            else:
+                return 'unknown_status'
+        finally:
+            socket.setdefaulttimeout(None)
