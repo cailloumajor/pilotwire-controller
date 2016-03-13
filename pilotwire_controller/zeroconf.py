71fd0da4046b22464fdf8dddcd2eedc41d840185
Arnaud Rocher <arnaud.roche3@gmail.com>
2016-03-13 21:28:21 +0100

    Fix typo : constant name not in uppercase

--- pilotwire_controller/zeroconf.py
+++ pilotwire_controller/zeroconf.py
@@ -9,7 +9,7 @@ from zeroconf import Zeroconf, ServiceInfo
 NAME = "Pilotwire Controller"
 SRV_TYPE = '_xml-rpc._tcp.local.'
 
-srv_fqname = '.'.join((NAME, SRV_TYPE))
+SRV_FQNAME = '.'.join((NAME, SRV_TYPE))
 
 
 class ZeroconfServiceNotFound(Exception):
@@ -24,7 +24,7 @@ class ServiceDiscoveryServer:
         addr = socket.inet_aton(addr)
         self.zeroconf = Zeroconf()
         self.info = ServiceInfo(
-            SRV_TYPE, srv_fqname, addr, port, 0, 0,
+            SRV_TYPE, SRV_FQNAME, addr, port, 0, 0,
             {}, '{}.local.'.format(hostname)
         )
 
@@ -40,7 +40,7 @@ class ServiceDiscoveryClient:
 
     def __init__(self, service_timeout=None):
         self.zeroconf = Zeroconf()
-        self.service_args = [SRV_TYPE, srv_fqname]
+        self.service_args = [SRV_TYPE, SRV_FQNAME]
         if service_timeout:
             self.service_args.append(service_timeout)
 
