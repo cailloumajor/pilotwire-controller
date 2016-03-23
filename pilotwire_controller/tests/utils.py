2d953965c64deecb25517968ae3c253d988b47b4
Arnaud Rocher <arnaud.roche3@gmail.com>
2016-03-23 15:43:13 +0100

    Move TestingController class out of tests package

--- pilotwire_controller/tests/utils.py
+++ pilotwire_controller/tests/utils.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from ..controller.base import BaseController
-
 
 output_to_modes = (
     (0,           {'1':'C', '2':'C', '3':'C', '4':'C'}),
@@ -14,17 +12,3 @@ modes_to_output = (
     ({'1':'C', '2':'E', '3':'H', '4':'A'}, 0b10011100),
     ({'4':'E', 'X':'Z'}, 0b11000000),
 )
-
-
-class TestingController(BaseController):
-
-    def __init__(self):
-        self._out = 0
-
-    @property
-    def output_value(self):
-        return self._out
-
-    @output_value.setter
-    def output_value(self, val):
-        self._out = val
