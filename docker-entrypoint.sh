e74ed7c174593d8dd8a13d377cfc3ce4ae4ec8c9
Arnaud Rocher <arnaud.roche3@gmail.com>
2018-09-11 22:44:37 +0200

    Fix permissions for SPI and GPIO use

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# Fix permissions to use SPI & GPIO
+chown -R root:resinapp /dev/spidev* /sys/class/gpio
+chmod g+rw /dev/spidev*
+chmod g+w /sys/class/gpio/*export
+
 if [ "$1" = "./run_controller.py" ] && [ "$(id -u)" = "0" ]; then
     chown -R resinapp .
     exec su-exec resinapp "$@"
