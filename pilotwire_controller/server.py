09458dbe5db942ca046cb50a1823125beb9b6bd8
Arnaud Rocher <arnaud.roche3@gmail.com>
2018-09-15 21:34:39 +0200

    Use a cached property for PiFaceController.
    This avoids to instantiate PiFaceDigital on each connection to the
    service.

--- pilotwire_controller/server.py
+++ pilotwire_controller/server.py
@@ -1,15 +1,16 @@
 import logging
 
 import rpyc
+from cached_property import threaded_cached_property
 
 from pilotwire_controller.piface import PiFaceController
 
 
 # pylint: disable=attribute-defined-outside-init
 class PilotwireControllerService(rpyc.Service):
-    def on_connect(self, conn):
-        self.controller = PiFaceController()
-        print()
+    @threaded_cached_property
+    def controller(self):
+        return PiFaceController()
 
     def exposed_get_modes(self):
         return self.controller.modes
